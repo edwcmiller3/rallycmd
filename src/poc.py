80b60aa08907131bb9007827b423386e75f9b2c4
edwcmiller3 <edwcmiller3@outlook.com>
2021-11-25 23:39:07 -0500

    basic usage working

--- src/poc.py
+++ src/poc.py
@@ -6,5 +6,14 @@ from pyral import Rally, rallyWorkset
 
 options = [opt for opt in sys.argv[1:] if opt.startswith('--')]
 server, user, password, apikey, workspace, project = rallyWorkset(options)
+rally = Rally(server, user, password, workspace=workspace, project=project)
 
-print(" ".join('|%s|' % opt for opt in [server, user, password, apikey, workspace, project]))
+# print(" ".join('|%s|' % opt for opt in [server, user, password, apikey, workspace, project]))
+
+query_criteria = 'FormattedID = "TC5535"'
+response = rally.get('TestCase', fetch=True, query=query_criteria)
+if response.errors:
+    sys.stdout.write("\n".join(errors))
+    sys.exit(1)
+for testCase in response:  # there should only be one qualifying TestCase
+    print(f"{testCase.Name}, {testCase.Type}, {testCase.LastVerdict}")
