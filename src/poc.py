d946ecc5c328c35b4f7ce8ad53dbb9f1ac46b6c2
edwcmiller3 <edwcmiller3@outlook.com>
2021-11-27 01:20:20 -0500

    hashing out basic use case

--- src/poc.py
+++ src/poc.py
@@ -16,3 +16,15 @@ if response.errors:
     sys.exit(1)
 for testCase in response:  # there should only be one qualifying TestCase
     print(f"{testCase.Name}, {testCase.Type}, {testCase.LastVerdict}")
+
+# Get task names for story
+owner_name = "Eddie" # DISPLAY NAME
+# target_owner = rally.getUserInfo(username='edward.miller@acatoim.com') 
+# OR 
+# target_owner = rally.getUserInfo(name=owner_name)
+
+
+story = rally.get('HierarchicalRequirement', fetch=True, query='FormattedID = "US16436"', instance=True)
+for task in story.Tasks:
+    if task.Owner.DisplayName == owner_name:
+        print(task.FormattedID, task.Name, task.State)
