a358fb3b168baa17b72971978460a8b57e72ae0f
edwcmiller3 <edwcmiller3@outlook.com>
2021-11-28 13:39:52 -0500

    fixing formatting

--- src/poc.py
+++ src/poc.py
@@ -1,6 +1,7 @@
 # Proof of concept to check modules working and
 # project setup correctly
 
+from os import sep
 import sys
 from pyral import Rally, rallyWorkset
 
@@ -17,13 +18,14 @@ rally = Rally(server, user, password, workspace=workspace, project=project)
 # for testCase in response:  # there should only be one qualifying TestCase
 #     print(f"{testCase.Name}, {testCase.Type}, {testCase.LastVerdict}")
 
-story = rally.get('HierarchicalRequirement', fetch=True, query='FormattedID = "US16436"', instance=True)
+columns = ["ID", "State", "Name", "Actuals"]
+story = rally.get('HierarchicalRequirement', fetch=True,
+                  query='FormattedID = "US16436"', instance=True)
 print(story.Name)
+
+# my_tasks = list of pyral Task objects
 my_tasks = list(filter(lambda x: x.Owner.EmailAddress == user, story.Tasks))
-columns = ["ID", "State", "Name", "Actuals (h)", "To Do (h)"]
-# my_task_details = [my_tasks.FormattedID, my_tasks.State, my_tasks.Name, my_tasks.Actuals, my_tasks.ToDo]
-a = [[task.FormattedID, task.State, task.Name, task.Actuals, task.ToDo] for task in my_tasks]
-formatting = "{:<9} {:<10} {:<35} {:<12} {:<12}"
+
+formatting = "{:<15} {:<15} {:<35} {:>12}"
 print(formatting.format(*columns))
-print(formatting.format(*a))
-# [print(task.FormattedID, task.State, task.Name, task.Actuals, task.ToDo) for task in my_tasks]
+[print(formatting.format(task.FormattedID, task.State, task.Name, task.Actuals)) for task in my_tasks]
