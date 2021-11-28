ad4ce6062211155e45f0e3eb96a180c579576685
edwcmiller3 <edwcmiller3@outlook.com>
2021-11-28 01:04:00 -0500

    baby steps and adding do nothing test

--- src/poc.py
+++ src/poc.py
@@ -10,21 +10,17 @@ rally = Rally(server, user, password, workspace=workspace, project=project)
 
 # print(" ".join('|%s|' % opt for opt in [server, user, password, apikey, workspace, project]))
 
-query_criteria = 'FormattedID = "TC5535"'
-response = rally.get('TestCase', fetch=True, query=query_criteria)
-if response.errors:
-    sys.exit(1)
-for testCase in response:  # there should only be one qualifying TestCase
-    print(f"{testCase.Name}, {testCase.Type}, {testCase.LastVerdict}")
-
-# Get task names for story
-owner_name = "Eddie" # DISPLAY NAME
-# target_owner = rally.getUserInfo(username='edward.miller@acatoim.com') 
-# OR 
-# target_owner = rally.getUserInfo(name=owner_name)
-
+# query_criteria = 'FormattedID = "TC5535"'
+# response = rally.get('TestCase', fetch=True, query=query_criteria)
+# if response.errors:
+#     sys.exit(1)
+# for testCase in response:  # there should only be one qualifying TestCase
+#     print(f"{testCase.Name}, {testCase.Type}, {testCase.LastVerdict}")
 
 story = rally.get('HierarchicalRequirement', fetch=True, query='FormattedID = "US16436"', instance=True)
-for task in story.Tasks:
-    if task.Owner.DisplayName == owner_name:
-        print(task.FormattedID, task.Name, task.State)
+print(story.Name)
+my_tasks = list(filter(lambda x: x.Owner.EmailAddress == user, story.Tasks))
+columns = ["ID", "Name", "State", "Actuals (h)", "To Do (h)"]
+print("{:<8} {:<15} {:<20} {:<10} {:<10}".format(*columns))
+
+[print(task.FormattedID, task.Name, task.State, task.Actuals, task.ToDo) for task in my_tasks]
---
+++ tests/blah.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_blah():
+    assert True
